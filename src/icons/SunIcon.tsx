7d64fb75df8725ef3bec1cd538bae699cebd7a19
e-newton <ericnewton2251@gmail.com>
2023-05-05 19:38:55 -0700

    Added scss, added in sun icon

---
+++ src/icons/SunIcon.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SunIcon.scss'
+import sunCore from './icon-images/Sun Core.svg'
+import sunBeamsXY from './icon-images/Sun Beams XY.svg'
+import sunBeamsDiag from './icon-images/Sun Beams Diag.svg'
+
+export default function SunIcon() {
+  return (
+    <div className='container'>
+      <img className='sun-core' src={sunCore} alt={'Sun Core'} />
+      <img className='sun-beams' src={sunBeamsXY} alt={'Sun Beams'}/>
+      <img className='sun-beams diag' src={sunBeamsDiag} alt={'Sun Beams'}/>
+    </div>
+  )
+}
