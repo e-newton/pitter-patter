1ae7e74e8af815c9dca6be801b676aa1221d7bd7
e-newton <ericnewton2251@gmail.com>
2023-06-11 14:04:31 -0700

    Added forecast item

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './Forecast.scss';
+import ForecastItem from './ForecastItem';
 
 export default function Forecast(): JSX.Element {
   return (
   <div className="forecast-container">
+    <ForecastItem/>
+    <ForecastItem/>
+    <ForecastItem/>
+    <ForecastItem/>
+    <ForecastItem/>
+    <ForecastItem/>
+    <ForecastItem/>
+    <ForecastItem/>
+    <ForecastItem/>
+    <ForecastItem/>
   </div>
   );
 }
--- src/components/ForecastItem.tsx
+++ src/components/ForecastItem.tsx
@@ -1,1 +1,19 @@
 import React from 'react';
+import Icon from '../icons/Icon';
+import { IconType } from '../icons/IconTypes';
+import './ForecastItem.scss';
+
+export default function ForecastItem(): JSX.Element {
+  return (
+  <div className='forecast-item'>
+    <span>Sunday</span>
+    <div className='icon-container'>
+      <Icon icon={IconType.Rain}/>
+    </div>
+    <div className="high-low">
+      <span className="low">L: 10˚</span>
+      <span className="high">H: 18˚</span>
+    </div>
+  </div>
+  );
+}
